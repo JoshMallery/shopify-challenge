10afb7c64111c1b05ade836f9545794d76a459e4
Josh Mallery <josh.mallery@gmail.com>
2022-05-11 11:28:30 -0600

    Add basic Fetch request info

--- src/App.js
+++ src/App.js
@@ -16,7 +16,28 @@ class App extends Component {
   }
 
 
+requestResponse = (prompt) => {
+  const data = {
+ prompt: prompt,
+ temperature: 0.5,
+ max_tokens: 64,
+ top_p: 1.0,
+ frequency_penalty: 0.0,
+ presence_penalty: 0.0,
+};
 
+fetch("https://api.openai.com/v1/engines/text-curie-001/completions", {
+ method: "POST",
+ headers: {
+   "Content-Type": "application/json",
+   Authorization: `Bearer ${process.env.OPENAI_SECRET}`,
+ },
+ body: JSON.stringify(data),
+})
+.then(data=>console.log(data))
+
+;
+}
 
 
 addResponse = (prompt,response) => {
